1012b34c349b9ed690b0e2b66b352136c38f3fea
martoonio <18221011@std.stei.itb.ac.id>
2023-10-04 00:55:15 +0700

    chore: rapiin

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 from config import Config
 import streamlit as st
+from streamlit_option_menu import option_menu
 import utils
 
 # Import config from json
@@ -15,49 +16,77 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# Sidebar
 
+# Sidebar
+st.sidebar.markdown(f"<img src=\"{utils.replace_image('assets/logo.png', 'img')}\" alt=\"Logo\" width=\"100\">", unsafe_allow_html=True)
+with st.sidebar.expander("About"):
+    st.markdown("""
+    This app is created to help students of Institut Teknologi Bandung to find their courses easily.
+    """)
 
 # Header
 with st.container():
-    c1, c2 = st.columns(2)
-    
-    c1.markdown(f"<img src=\"{utils.replace_image('assets/logo.png', 'img')}\" alt=\"Logo\" width=\"200\">", unsafe_allow_html=True)
-    
-    c2.markdown("")
-    c2.markdown("")
-    c2.markdown("")
-    select_bar = c2.selectbox("Select Faculty", fakultas)
+    c1, c2, c3 = st.columns(3)    
+    c1.markdown(f"<img src=\"{utils.replace_image('assets/logo.png', 'img')}\" alt=\"Logo\" width=\"150\">", unsafe_allow_html=True)
+    select_bar = c2.selectbox("Pilih Fakultas : ", fakultas)
+
+# Daftar tahun
+tahun_range = list(range(2018, 2023))
+
+# Daftar label untuk tiap tahun
+tahun_labels = {2018: 'Tahun 2018', 2019: 'Tahun 2019', 2020: 'Tahun 2020', 2021: 'Tahun 2021', 2022: 'Tahun 2022'}
+
+# Membuat slider tahun
+tahun_terpilih = st.slider('', min_value=min(tahun_range), max_value=max(tahun_range))
+
 
 # Main
 with st.container():
-    select_button = st.button("Select")
-    homepage_button = st.button("Go to Homepage")
-
-    if select_button:
         if select_bar == fakultas[0]:
-            inputProdi = st.selectbox('Program Studi', prodi) #Ini harusnya multipage
+            inputProdi = c3.selectbox('Program Studi', prodi) #Ini harusnya multipage
         elif select_bar == fakultas[1]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Ilmu dan Teknologi Kebumian'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas'][fakultas[1]])))
         elif select_bar == fakultas[2]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Matematika dan Ilmu Pengetahuan Alam'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Matematika dan Ilmu Pengetahuan Alam'])))
         elif select_bar == fakultas[3]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Seni Rupa dan Desain'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Seni Rupa dan Desain'])))
         elif select_bar == fakultas[4]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknologi Industri'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknologi Industri'])))
         elif select_bar == fakultas[5]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Sipil dan Lingkungan'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Sipil dan Lingkungan'])))
         elif select_bar == fakultas[6]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Mesin dan Dirgantara'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Mesin dan Dirgantara'])))
         elif select_bar == fakultas[7]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Pertambangan dan Perminyakan'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Fakultas Teknik Pertambangan dan Perminyakan'])))
         elif select_bar == fakultas[8]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Arsitektur, Perencanaan dan Pengembangan Kebijakan'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Arsitektur, Perencanaan dan Pengembangan Kebijakan'])))
         elif select_bar == fakultas[9]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Bisnis dan Manajemen'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Bisnis dan Manajemen'])))
         elif select_bar == fakultas[10]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Farmasi'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Farmasi'])))
         elif select_bar == fakultas[11]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Ilmu dan Teknologi Hayati'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Ilmu dan Teknologi Hayati'])))
         elif select_bar == fakultas[12]:
-            inputProdi = st.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Teknik Elektro dan Informatika'])))
+            inputProdi = c3.selectbox('Program Studi', sorted(list(cfg['fakultas']['Sekolah Teknik Elektro dan Informatika'])))
+
+selected = option_menu(
+        menu_title=None,
+        options=kategori,
+        default_index=0,
+        orientation="horizontal",
+        styles={
+            "container": {"padding": "0!important"},
+            "nav-link": {
+                "font-size": "14px",
+                "font-weight": "regular",
+                "text-align": "left",
+                "margin": "0px",
+                "--hover-color": "#BF9849",
+            },  
+            "nav-link-selected": {
+                "background-color": "#BF9849",
+            }
+        },
+)
+
+st.write(selected,select_bar, inputProdi, tahun_terpilih)
