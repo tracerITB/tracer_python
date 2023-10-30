14a7931f5ad86182ef1a60d0085d0cab327ebb1c
safiqq <szarifin20041@gmail.com>
2023-10-30 12:45:22 +0700

    feat: create parent class for babs

---
+++ src/bab.py
@@ -0,0 +1,6 @@
+class Bab:
+    def __init__(self, input_tahun, input_fakultas, input_prodi, dataframe):
+        self.input_tahun = input_tahun
+        self.input_fakultas = input_fakultas
+        self.input_prodi = input_prodi
+        self.dataframe = dataframe
