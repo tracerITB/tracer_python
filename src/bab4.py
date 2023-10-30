f0b87706b0f3c05c4dd3821531691d46f527cb39
safiqq <szarifin20041@gmail.com>
2023-10-30 12:47:08 +0700

    feat: update progress bab4

--- src/bab4.py
+++ src/bab4.py
@@ -1,15 +1,26 @@
 import streamlit as st
 import plotly.express as px
-import pandas as pd
+from src.bab import Bab
 
 
-class BabEmpat:
+
+class BabEmpat(Bab):
     def __init__(self, input_tahun, input_fakultas, input_prodi, dataframe):
-        self.input_tahun = input_tahun
-        self.input_fakultas = input_fakultas
-        self.input_prodi = input_prodi
-        self.dataframe = dataframe
-        self.valid_categories = ["multinasional", "nasional", "lokal"]
+        super().__init__(input_tahun, input_fakultas, input_prodi, dataframe)
+        self.kategori_perusahaan = ["multinasional", "nasional", "lokal"]
+        self.bentuk_perusahaan = [
+            "instansi pemerintah",
+            "BUMN",
+            "organisasi non-profit / lembaga swadaya masyarakat",
+            "perusahaan swasta",
+            "wiraswasta / perusahaan sendiri",
+        ]
+
+    def show(self):
+        self.showKategoriPerusahaanPerProdi()
+        self.showBentukPerusahaanTempatBekerja()
+        self.showWaktuMemulaiUsaha()
+        self.showWaktuMemulaiUsahaPersebaran()
 
     def showKategoriPerusahaanPerProdi(self):
         # All
@@ -18,7 +29,7 @@ class BabEmpat:
             st.write("Kategori Perusahaan per Prodi")
             df = self.dataframe[
                 self.dataframe["Apa kategori perusahaan tempat Anda bekerja?"].isin(
-                    self.valid_categories
+                    self.kategori_perusahaan
                 )
             ]
             pivot_df = df.pivot_table(
@@ -30,7 +41,7 @@ class BabEmpat:
             pivot_df_percentage = pivot_df.div(pivot_df.sum(axis=1), axis=0) * 100
             fig = px.bar(
                 pivot_df_percentage,
-                x=self.valid_categories,
+                x=self.kategori_perusahaan,
                 labels={"value": "Persentase"},
             )
 
@@ -70,7 +81,7 @@ class BabEmpat:
             ]
             df = df[
                 df["Apa kategori perusahaan tempat Anda bekerja?"].isin(
-                    self.valid_categories
+                    self.kategori_perusahaan
                 )
             ]
             pivot_df = df.pivot_table(
@@ -82,7 +93,7 @@ class BabEmpat:
             pivot_df_percentage = pivot_df.div(pivot_df.sum(axis=1), axis=0) * 100
             fig = px.bar(
                 pivot_df_percentage,
-                x=self.valid_categories,
+                x=self.kategori_perusahaan,
                 labels={"value": "Persentase"},
             )
 
@@ -118,9 +129,7 @@ class BabEmpat:
             st.write("")
             st.write("Kategori Perusahaan per Prodi")
             df = self.dataframe[self.dataframe["Program Studi"] == self.input_prodi]
-            df = df["Apa kategori perusahaan tempat Anda bekerja?"]
-            x = ["multinasional", "nasional", "lokal"]
-            df = df[df.isin(x)]
+            df = df["Apa kategori perusahaan tempat Anda bekerja?"].isin(self.kategori_perusahaan)
             fig = px.bar(
                 df,
                 x=df.value_counts().tolist(),
@@ -146,4 +155,118 @@ class BabEmpat:
                 )
             fig.update_layout(annotations=annotations)
 
+            st.plotly_chart(fig, use_container_width=True)
+
+    def showBentukPerusahaanTempatBekerja(self):
+        # All
+        if self.input_fakultas == "All":
+            st.write("")
+            st.write("Bentuk Perusahaan Tempat Bekerja")
+            df = self.dataframe[
+                self.dataframe["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].isin(
+                    self.bentuk_perusahaan
+                )
+            ]
+            df = df["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Fakultas
+        elif self.input_fakultas != "All" and self.input_prodi == "All":
+            st.write("")
+            st.write("Bentuk Perusahaan Tempat Bekerja")
+            df = self.dataframe[
+                self.dataframe["Fakultas/Sekolah"] == self.input_fakultas
+            ]
+            df = df[
+                df["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].isin(
+                    self.bentuk_perusahaan
+                )
+            ]
+            df = df["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Program studi
+        else:
+            st.write("")
+            st.write("Bentuk Perusahaan Tempat Bekerja")
+            df = self.dataframe[
+                self.dataframe["Program Studi"] == self.input_prodi
+            ]
+            df = df[
+                df["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].isin(
+                    self.bentuk_perusahaan
+                )
+            ]
+            df = df["Apa jenis perusahaan / instansi / institusi tempat Anda bekerja sekarang?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+    def showWaktuMemulaiUsaha(self):
+        # All
+        if self.input_fakultas == "All":
+            st.write("")
+            st.write("Waktu Memulai Usaha")
+            df = self.dataframe["Kapankah Anda memulai usaha?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Fakultas
+        elif self.input_fakultas != "All" and self.input_prodi == "All":
+            st.write("")
+            st.write("Waktu Memulai Usaha")
+            df = self.dataframe[
+                self.dataframe["Fakultas/Sekolah"] == self.input_fakultas
+            ]
+            df = df["Kapankah Anda memulai usaha?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Program studi
+        else:
+            st.write("")
+            st.write("Waktu Memulai Usaha")
+            df = self.dataframe[
+                self.dataframe["Program Studi"] == self.input_prodi
+            ]
+            df = df["Kapankah Anda memulai usaha?"].value_counts()
+            fig = px.pie(df, values=df.tolist(), names=df.index.tolist())
+            st.plotly_chart(fig, use_container_width=True)
+
+    def showWaktuMemulaiUsahaPersebaran(self):
+        # All
+        if self.input_fakultas == "All":
+            df = self.dataframe[["Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?", "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?"]]
+            df = df.rename(columns={
+                "Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?": "sebelum lulus",
+                "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?": "setelah lulus"
+            })
+            fig = px.box(df, title="Waktu Memulai Usaha (Persebaran)")
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Fakultas
+        elif self.input_fakultas != "All" and self.input_prodi == "All":
+            df = self.dataframe[
+                self.dataframe["Fakultas/Sekolah"] == self.input_fakultas
+            ]
+            df = df[["Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?", "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?"]]
+            df = df.rename(columns={
+                "Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?": "sebelum lulus",
+                "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?": "setelah lulus"
+            })
+            fig = px.box(df, title="Waktu Memulai Usaha (Persebaran)")
+            st.plotly_chart(fig, use_container_width=True)
+
+        # Program studi
+        else:
+            df = self.dataframe[
+                self.dataframe["Program Studi"] == self.input_prodi
+            ]
+            df = df[["Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?", "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?"]]
+            df = df.rename(columns={
+                "Berapa bulan waktu yang digunakan (sebelum kelulusan) untuk memulai usaha?": "sebelum lulus",
+                "Berapa bulan waktu yang digunakan (sesudah kelulusan) untuk memulai usaha?": "setelah lulus"
+            })
+            fig = px.box(df, title="Waktu Memulai Usaha (Persebaran)")
             st.plotly_chart(fig, use_container_width=True)
